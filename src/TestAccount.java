7d5904147ba4963d921bb3882c0988f7b2e1a9b6
M Sullivan <ms1869@gmail.com>
2017-02-11 14:48:06 -0500

    first pass for Account methods

--- src/TestAccount.java
+++ src/TestAccount.java
@@ -3,5 +3,10 @@
  */
 public class TestAccount
 {
-    
+    public static void main(String[] args)
+    {
+        Account a1 = new CheckingAccount("a1234", "Halston", 2500.00);
+        a1.deposit(500.00);
+        a1.withdraw(2500.00);
+    }
 }
